b21b0f355c59fdd25230dc11bd0e79fdce1f2287
Mauricio Arroyo <mauricioarroyo@ascbomacmini76.local>
2025-05-19 16:59:20 -0400

    added more accurete latitudes and longitudes

--- script.js
+++ script.js
@@ -32,8 +32,8 @@ const CITIES = [
   {
     id: 6,
     name: "Pando",
-    latitude: -11.0184614,
-    longitude: -68.7538149,
+    latitude: -11.0267,
+    longitude: -68.7692,
   },
   {
     id: 7,
@@ -50,8 +50,8 @@ const CITIES = [
   {
     id: 9,
     name: "Sucre",
-    latitude: -19.0477251,
-    longitude: -65.2594306,
+    latitude: -19.0333,
+    longitude: -65.2627,
   },
   {
     id: 10,
@@ -63,6 +63,7 @@ const CITIES = [
 const UNITS = "metric";
 const API_KEY = "101577224ac8d22081f12ebb7a0e3d7a";
 const LANGUAGE = "es";
+const selectEl = document.getElementsByClassName("select")[0];
 let weatherData = null;
 
 async function getWeather(latitude, longitude) {
@@ -72,26 +73,10 @@ async function getWeather(latitude, longitude) {
   return data;
 }
 
-const selectEl = document.getElementsByClassName("select")[0];
-function pupulateSelectEl() {
-  CITIES.forEach((city) => {
-    const optionEl = document.createElement("option");
-    optionEl.classList.add("option");
-    optionEl.value = city.id;
-    optionEl.textContent = city.name;
-    selectEl.appendChild(optionEl);
-  });
-}
-pupulateSelectEl();
-selectEl.addEventListener("change", async function (event) {
-  const selectedId = parseInt(event.target.value);
-  const selectedCity = CITIES.find((city) => city.id === selectedId);
-
-  weatherData = await getWeather(selectedCity.latitude, selectedCity.longitude);
-  renderWeather();
-});
-
 function renderWeather() {
+  const cardBodyDataEl = document.getElementsByClassName("card__body__data")[0];
+  cardBodyDataEl.classList.add("card__body__data-shown");
+
   const weatheIconEl = document.getElementsByClassName("icon")[0];
   weatheIconEl.src = `https://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png`;
 
@@ -117,9 +102,33 @@ function renderWeather() {
   weatherDescriptionEl.textContent = weatherData.weather[0].description;
 
   const humidityEl = document.getElementsByClassName("humidity")[0];
-  humidityEl.textContent = `Humedad: ${weatherData.main.humidity}%` ;
-
+  humidityEl.textContent = `Humedad: ${weatherData.main.humidity}%`;
 
   const windEl = document.getElementsByClassName("wind")[0];
   windEl.textContent = `Viento: a ${parseInt(weatherData.wind.speed)} km/h`;
 }
+
+(function pupulateSelectElement() {
+  CITIES.forEach((city) => {
+    const optionEl = document.createElement("option");
+    optionEl.classList.add("option");
+    optionEl.value = city.id;
+    optionEl.textContent = city.name;
+    selectEl.appendChild(optionEl);
+  });
+})();
+
+selectEl.addEventListener("change", async function (event) {
+  const cardBodyDataEl = document.getElementsByClassName("card__body__data")[0];
+  cardBodyDataEl.classList.remove("card__body__data-shown");
+  const cardSpinnerEl = document.getElementsByClassName(
+    "card__body__spinner"
+  )[0];
+  cardSpinnerEl.classList.add("card__body__spinner-shown");
+  const selectedId = parseInt(event.target.value);
+  const selectedCity = CITIES.find((city) => city.id === selectedId);
+  await new Promise(res => setTimeout(res, 800));
+  weatherData = await getWeather(selectedCity.latitude, selectedCity.longitude);
+  renderWeather();
+  cardSpinnerEl.classList.remove("card__body__spinner-shown");
+});
